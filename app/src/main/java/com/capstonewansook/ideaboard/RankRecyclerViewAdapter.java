5f9dffeb837fb2d326b9f7882e0c016f27b04501
tmsgod2 <tmsgod2@naver.com>
2020-04-07 21:43:50 +0900

    랭킹 리사이클러뷰 추가

---
+++ app/src/main/java/com/capstonewansook/ideaboard/RankRecyclerViewAdapter.java
@@ -0,0 +1,102 @@
+package com.capstonewansook.ideaboard;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RankRecyclerViewAdapter extends RecyclerView.Adapter<RankRecyclerViewAdapter.ViewHolder> {
+    private ArrayList<RankRecyclerViewData> mData = null;
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView profileImage;
+        TextView nameText,ofiiceText,starText,manyStarText;
+
+        ViewHolder(View itemView){
+            super(itemView);
+
+            profileImage = itemView.findViewById(R.id.rank_re_profile_imageView);
+            nameText = itemView.findViewById(R.id.rank_re_name_textView);
+            ofiiceText = itemView.findViewById(R.id.rank_re_office_textView);
+            starText = itemView.findViewById(R.id.rank_re_star_textView);
+            manyStarText = itemView.findViewById(R.id.rank_re_manyStar_textView);
+            profileImage.setBackground(new ShapeDrawable((new OvalShape())));
+            profileImage.setClipToOutline(true);
+
+        }
+
+    }
+
+    RankRecyclerViewAdapter(ArrayList<RankRecyclerViewData> list){
+        mData = list;
+        Collections.sort(mData);
+        for(int i = 0; i<mData.size();i++){
+            mData.get(i).setGrade(i+1);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RankRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.ranking_recyclerview, parent, false);
+        RankRecyclerViewAdapter.ViewHolder vh = new RankRecyclerViewAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RankRecyclerViewAdapter.ViewHolder holder, int position) {
+        String name = mData.get(position).getName();
+        String office = mData.get(position).getOffice();
+        int manyStar = mData.get(position).getStar();
+        int profile = mData.get(position).getProfileImage();
+        int grade = mData.get(position).getGrade();
+
+        holder.nameText.setText(name);
+        holder.ofiiceText.setText(office);
+        holder.manyStarText.setText(String.valueOf(manyStar));
+        //이거 바꿔야 할 수도 있음
+        holder.profileImage.setImageResource(profile);
+        holder.starText.setText(String.valueOf(grade));
+
+        switch (grade){
+            case 1:
+                holder.starText.setBackgroundResource(R.drawable.ic_star_gold_24dp);
+                break;
+            case 2:
+                holder.starText.setBackgroundResource(R.drawable.ic_star_silver_24dp);
+                break;
+            case 3:
+                holder.starText.setBackgroundResource(R.drawable.ic_star_bronze_24dp);
+                holder.starText.setTextColor(Color.WHITE);
+                break;
+            default:
+                holder.starText.setBackgroundResource(R.drawable.ic_star_black_ranking);
+                holder.starText.setTextColor(Color.WHITE);
+                break;
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/capstonewansook/ideaboard/RankRecyclerViewData.java
@@ -0,0 +1,61 @@
+package com.capstonewansook.ideaboard;
+
+public class RankRecyclerViewData implements Comparable<RankRecyclerViewData>{
+    private int profileImage;
+    private String name;
+    private String office;
+    private int star;
+    private int grade=99999;
+
+    public RankRecyclerViewData(int profileImage, String name, String office, int star) {
+        this.profileImage = profileImage;
+        this.name = name;
+        this.office = office;
+        this.star = star;
+    }
+
+    public int getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(int profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public int compareTo(RankRecyclerViewData rankRecyclerViewData) {
+        return rankRecyclerViewData.star - this.star;
+    }
+}
