c5e403ba60b345f45131a392dd54292183c0811a
tmsgod2 <tmsgod2@naver.com>
2020-04-14 15:28:03 +0900

    오류 수정 및 확인

--- app/src/main/java/com/capstonewansook/ideaboard/logintitle.java
+++ app/src/main/java/com/capstonewansook/ideaboard/logintitle.java
@@ -1,4 +1,15 @@
 package com.capstonewansook.ideaboard;
 
-public class logintitle {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class logintitle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logintitle);
+    }
 }
