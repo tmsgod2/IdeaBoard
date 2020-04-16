4116bf4a2f4d0d00bb803de4d7d98a64e3c3bc25
SeongJaeLee-lsj <lce666@naver.com>
2020-04-16 19:08:16 +0900

    아이디어 창 푸쉬

---
+++ app/src/main/java/com/capstonewansook/ideaboard/IdeaFragment1.java
@@ -0,0 +1,53 @@
+package com.capstonewansook.ideaboard;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.capstonewansook.ideaboard.recyclerview.IdeaRecyclerViewAdapter;
+import com.capstonewansook.ideaboard.recyclerview.IdeaRecycletViewData;
+
+import java.util.ArrayList;
+
+public class IdeaFragment1 extends Fragment {
+    private View view;
+    private RecyclerView rv;
+    private TextView tv;
+
+    private ArrayList<IdeaRecycletViewData> arrayList;
+    private IdeaRecyclerViewAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.ideafragment,container,false);
+        rv=view.findViewById(R.id.rv_newidea);
+        Context context = view.getContext();
+
+
+
+
+        recyclerView = view.findViewById(R.id.rv_newidea);
+        linearLayoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+
+        mainAdapter = new IdeaRecyclerViewAdapter(arrayList);
+        recyclerView.setAdapter(mainAdapter);
+        arrayList.add( new IdeaRecycletViewData(R.drawable.idea_profile,"안드러이드","2000.1.1",R.drawable.heart,R.drawable.comment,"13","10"));
+
+        return view;
+    }
+}
