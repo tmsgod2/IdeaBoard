6081fdaa00fea7e783ae9c04da33cc3fd86a7a2d
tmsgod2 <tmsgod2@naver.com>
2020-05-28 22:20:28 +0900

    알람기능 추가

--- app/src/main/java/com/capstonewansook/ideaboard/ChatlistAdapter.java
+++ app/src/main/java/com/capstonewansook/ideaboard/ChatlistAdapter.java
@@ -12,10 +12,20 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChatlistAdapter extends BaseAdapter {
     private ArrayList<Chatlistitem> chatViewItemList = new ArrayList<Chatlistitem>() ;
+    private String roomId;
+    private boolean noAlram;
     public ChatlistAdapter(){
 
     }
@@ -47,7 +57,7 @@ public class ChatlistAdapter extends BaseAdapter {
         ImageView iconImageView = (ImageView) convertView.findViewById(R.id.chatlist_image) ;
         TextView TextView = (TextView) convertView.findViewById(R.id.chatlist_textview1) ;
         ImageView iconImageView2 = (ImageView) convertView.findViewById(R.id.chatlist_image2) ;
-        CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.chatlistcheckbox) ;
+        final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.chatlistcheckbox) ;
 
         Chatlistitem chatlistitem = chatViewItemList.get(position);
 
@@ -55,7 +65,30 @@ public class ChatlistAdapter extends BaseAdapter {
         TextView.setText(chatlistitem.getTitleStr());
         iconImageView2.setImageDrawable(chatlistitem.getIconDrawable2());
         if(chatlistitem.isCheck() == 1) {
+            checkBox.setChecked(noAlram);
             checkBox.setVisibility(View.VISIBLE);
+            checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    db.collection("chatrooms").document(roomId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()){
+                                HashMap<String, Object> map = new HashMap<>();
+                                noAlram = !noAlram;
+                                if(task.getResult().get("uid1").toString().equals(MainActivity.uid)){
+                                    map.put("uid1NoSign",noAlram);
+                                }
+                                else{
+                                    map.put("uid2NoSign",noAlram);
+                                }
+                                db.collection("chatrooms").document(roomId).update(map);
+                            }
+                        }
+                    });
+                }
+            });
         }
         if(chatlistitem.isCheck() == 2){
             TextView.setTypeface(null, Typeface.BOLD);
@@ -84,18 +117,25 @@ public class ChatlistAdapter extends BaseAdapter {
     }
     public void addItem(Drawable icon, String title,Drawable icon2) {
         Chatlistitem item = new Chatlistitem();
-
         item.setCheck(0);
         item.setIconDrawable(icon);
         item.setIconDrawable2(icon2);
         item.setTitleStr(title);
-
-
         chatViewItemList.add(item);
-    }public void addItem(int i,String title) {
+    }
+    public void addItem(int i,String title) {
         Chatlistitem item = new Chatlistitem();
         item.setTitleStr(title);
         item.setCheck(i);
         chatViewItemList.add(item);
     }
+    public void addItem(int i , String title, String roomId, boolean noAlram){
+        Chatlistitem item = new Chatlistitem();
+        item.setTitleStr(title);
+        item.setCheck(i);
+        chatViewItemList.add(item);
+        this.roomId = roomId;
+        this.noAlram = noAlram;
+    }
+
 }
